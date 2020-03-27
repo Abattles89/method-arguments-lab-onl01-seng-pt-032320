c8266a464b2a473c0e7205c77b1cfc4c60a6e156
Ashleigh D Battles <blueangel365@gmail.com>
2020-03-27 01:37:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language("Ashleigh", "Ruby.rb")
+def introduction_with_language("Ashleigh","Ruby.rb")
   puts introduction
 end
